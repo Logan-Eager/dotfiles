06b0a9ab6de6133b0a8c67f4bffa8b1767487e5b
Logan <loganallaneager@gmail.com>
2026-07-09 10:55:28 +1200

    fixed updateChecker

--- waybar/updateChecker.sh
+++ waybar/updateChecker.sh
@@ -2,14 +2,15 @@
 
 archUpdates=$(checkupdates 2>/dev/null | wc -l)
 aurUpdates=$(yay -Qum 2>/dev/null | wc -l)
-totalUpdates=$((archUpdates + aurUpdates))
+totalUpdates=$(($archUpdates + $aurUpdates))
 
 if [ "$totalUpdates" -gt 0 ]; then
-    tooltipText="<b>Updates Available: $totalUpdates</b>\n\n"
-    tooltipText+="Arch Packages: $archUpdates\n"
-    tooltipText+="AUR Packages: $aurUpdates\n\n"
-    tooltipText+="<i>Click widget to update system</i>"
+    tooltipText="<b><u>Updates Available</u></b>\n"
+    tooltipText+="<b>Arch: $archUpdates\nAur: $aurUpdates</b>"
 
-    printf '{"text": "󰚰 %d", "tooltip": "%s"}\n' "$totalUpdates"
+    printf '{"text": "󰚰 %d", "tooltip": "%s"}\n' "$totalUpdates" "$tooltipText"
+
+else
+    printf '{"text": "", "tooltip": ""}\n'
 fi
 
